03d1efb18dadcd4bd5c0c01d2d65313bb5a459b9
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-12-08 18:30:23 +0200

    create admin panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Admin } from "react-admin";
+import buildHasuraProvider from "ra-data-hasura";
 
 function App() {
+  const [dataProvider, setDataProvider] = useState(null);
+
+  useEffect(() => {
+    const buildDataProvider = async () => {
+      const dataProvider = await buildHasuraProvider({
+        clientOptions: { uri: "http://localhost:8080/v1/graphql" },
+      });
+      setDataProvider(() => dataProvider);
+    };
+    buildDataProvider();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Admin />
+    </>
   );
 }
 
