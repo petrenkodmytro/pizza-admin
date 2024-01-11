577edb435cbd13af0897db3b07aef3a31f7ff330
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2024-01-11 18:09:06 +0200

    add new env and hasura cloud

--- src/core/apollo-clientn.ts
+++ src/core/apollo-clientn.ts
@@ -3,6 +3,8 @@ import { setContext } from "@apollo/client/link/context";
 import { JWT_ADMIN_TOKEN } from "./constants";
 
 const httpLink = createHttpLink({ uri: "http://localhost:8080/v1/graphql" });
+// https://assuring-kodiak-14.hasura.app/v1/graphql
+
 const authLink = setContext((_, config) => {
   const token = localStorage.getItem(JWT_ADMIN_TOKEN);
 
